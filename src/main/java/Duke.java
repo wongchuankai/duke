b1d8af610d916bc67a6c7d9b943cfa1efa81bfca
CK <wongchuankai97@gmail.com>
2019-08-27 10:15:59 +0800

    Level 8. Dates and Times

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -63,7 +63,16 @@ public class Duke {
                         String[] deadlinearr = deadlineString.split(" /by ");
                         System.out.println("    ____________________________________________________________");
                         System.out.println("     Got it. I've added this task:");
-                        Deadline deadline = new Deadline(deadlinearr[0], deadlinearr[1]);
+                String[] date = deadlinearr[1].split("/");
+                String[] tm = date[2].split(" ");
+                //
+                int day = Integer.parseInt(date[0]);
+                int month = Integer.parseInt(date[1]);
+                int year = Integer.parseInt(tm[0]);
+                String time = tm[1];
+                time t = new time(day, month, year, time);
+                //
+                        Deadline deadline = new Deadline(deadlinearr[0], t.toString());
                         taskArrayList.add(deadline);
                         int numberOfTask = taskArrayList.size();
                         System.out.println("       " + deadline.toString());
