43c7d1826503ca12a2b81eceb8276f02ccac5af6
CK <wongchuankai97@gmail.com>
2019-09-14 17:29:58 +0800

    sort changed

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -20,34 +20,41 @@ public class Sort extends Command{
     @Override
     public String execute(TaskList task, Ui ui, Storage storage) throws DukeException {
         String[] getSortingOrder = command.split(" ");
-        if (getSortingOrder[1].equals("1") || getSortingOrder[1].equals("desc")) {
-            String desc = "     Here are the tasks in your list:\n";
-            desc += "     Sorted according to desc of task.\n";
-            task.SortByDescription();
-            try {
-                storage.WriteToStorage(task);
-            } catch (IOException e) {
-                return "ERROR";
+        try {
+            if (getSortingOrder.length == 1) {
+                throw new DukeException(" sort according to 1(desc) or 2(type)");
             }
-            for (int i = 1; i <= task.getCount(); i++) {
-                desc += "     " + i + "." + task.getTaskList().get(i - 1) + "\n";
+            if (getSortingOrder[1].equals("1") || getSortingOrder[1].equals("desc")) {
+                String desc = "     Here are the tasks in your list:\n";
+                desc += "     Sorted according to desc of task.\n";
+                task.SortByDescription();
+                try {
+                    storage.WriteToStorage(task);
+                } catch (IOException e) {
+                    return "ERROR";
+                }
+                for (int i = 1; i <= task.getCount(); i++) {
+                    desc += "     " + i + "." + task.getTaskList().get(i - 1) + "\n";
+                }
+                return desc;
+            } else if (getSortingOrder[1].equals("2") || getSortingOrder[1].equals("type")) {
+                String desc = "     Here are the tasks in your list:\n";
+                desc += "     Sorted according to type of task.\n";
+                task.SortByType();
+                try {
+                    storage.WriteToStorage(task);
+                } catch (IOException e) {
+                    return "ERROR";
+                }
+                for (int i = 1; i <= task.getCount(); i++) {
+                    desc += "     " + i + "." + task.getTaskList().get(i - 1) + "\n";
+                }
+                return desc;
+            } else {
+                return "     Please Input the correct command.\n     sort<SPACE>1/2 only\n     or sort<SPACE>desc/type only";
             }
-            return desc;
-        } else if (getSortingOrder[1].equals("2") || getSortingOrder[1].equals("type")) {
-            String desc = "     Here are the tasks in your list:\n";
-            desc += "     Sorted according to type of task.\n";
-            task.SortByType();
-            try {
-                storage.WriteToStorage(task);
-            } catch (IOException e) {
-                return "ERROR";
-            }
-            for (int i = 1; i <= task.getCount(); i++) {
-                desc += "     " + i + "." + task.getTaskList().get(i - 1) + "\n";
-            }
-            return desc;
-        } else {
-            return "     Please Input the correct command.\n     sort<SPACE>1/2 only\n     or sort<SPACE>desc/type only";
+        } catch (DukeException e) {
+            return e.toString();
         }
     }
 }
